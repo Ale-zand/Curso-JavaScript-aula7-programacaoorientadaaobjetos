01c216eafb238729b11ff586380057230bb2e252
Ale-zand <ale_zand@hotmail.com>
2023-05-03 11:33:18 -0300

    #, get, set. Ecapsulamento, dados privados e assessors

--- Admin.js
+++ Admin.js
@@ -1,14 +1,17 @@
 import User from "./User.js";
 
-class Admin extends User {
+export default class Admin extends User {
     constructor(nome, email, nascimento, role = 'admin', ativo = true) {
-        super(nome, email, nascimento, role, ativo);
+        super(nome, email, nascimento, role, ativo)
     }
 
     criarCurso(nomeDoCurso, vagas) {
-        return `Curso de ${nomeDoCurso} criado com ${vagas} vagas.`;
+        return `Curso de ${nomeDoCurso} criado com ${vagas} vagas.`
     }
 }
 
-const novoAdmin = new Admin('Rodrigo', 'r@r.com', '2021-01-01');
-console.log(novoAdmin.criarCurso('JS', 20));
+// const novoAdmin = new Admin('Rodrigo', 'r@r.com', '2020-01-01')
+// // // console.log(novoAdmin.nome);
+// console.log(novoAdmin.exibirInfos())
+// // console.log(novoAdmin.role);
+// // console.log(Admin.nome);
---
+++ index.js
@@ -0,0 +1,18 @@
+import User from "./User.js";
+import Docente from "./Docente.js";
+import Admin from "./Admin.js";
+
+// const novoUser = new User('Mariana', 'm@m.com', '2021-01-01')
+// console.log(novoUser.exibirInfos())
+
+const novoAdmin = new Admin('Rodrigo', 'r@r.com', '2020-01-01')
+console.log(novoAdmin.nome)
+novoAdmin.nome = 'André'
+console.log(novoAdmin.nome)
+novoAdmin.nome = ''
+console.log(novoAdmin.nome)
+
+
+// novoUser.#nome = 'Márcia';
+// console.log(novoUser.#nome);
+
